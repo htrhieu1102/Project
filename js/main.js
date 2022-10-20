b79f66186df73bcf47d0906014a81a1f1acdff7b
Hoang Trung Hieu - 20130260 <20130260@st.hcmuaf.edu.vn>
2022-10-20 20:51:00 +0700

    Create header footer

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//show box search
+$("#icon-search").on('click', function () {
+    $("#search-container").css('display', 'block')
+})
+// hide box search
+$(".btn-close").on('click', function () {
+    $("#search-container").css('display', 'none')
+})
