32310b233c08f40809c247735159f99b2ff6648e
Hoang Trung Hieu - 20130260 <20130260@st.hcmuaf.edu.vn>
2022-10-28 16:44:08 +0700

    create css js sign in and sign up

--- js/main.js
+++ js/main.js
@@ -20,4 +20,24 @@ $(".btn-close").on('click', function () {
     $("#footer").css('background', 'rgb(0, 0, 0, 0)')
 })
 
-
+// switch tabs login and registration
+$('.sign-in-choose').on('click', function () {
+    $('.sign-in-container').css('display', 'block')
+    $('.sign-up-container').css('display', 'none')
+    $('.sign-in-choose').css('border-top', '2px solid #0f9d58')
+    $('.sign-in-choose').css('background', '#ffffff')
+    $('.sign-in-choose').css('opacity', '1')
+    $('.sign-up-choose').css('border-top', 'none')
+    $('.sign-up-choose').css('background', '#c2c7d0')
+    $('.sign-up-choose').css('opacity', '0.8')
+})
+$('.sign-up-choose').on('click', function () {
+    $('.sign-up-container').css('display', 'block')
+    $('.sign-in-container').css('display', 'none')
+    $('.sign-up-choose').css('border-top', '2px solid #0f9d58')
+    $('.sign-up-choose').css('background', '#ffffff')
+    $('.sign-up-choose').css('opacity', '1')
+    $('.sign-in-choose').css('border-top', 'none')
+    $('.sign-in-choose').css('background', '#c2c7d0')
+    $('.sign-in-choose').css('opacity', '0.8')
+})
