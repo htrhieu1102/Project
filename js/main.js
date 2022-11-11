1ef4ac8f9053c90dac7935f0dfed57edc5fa7351
unknown <20130262@st.hcmuaf.edu.vn>
2022-11-11 21:16:27 +0700

    Add detail description and rate from user

--- js/main.js
+++ js/main.js
@@ -81,3 +81,17 @@ $(".btn-close").on('click', function () {
 
         }
     })
+// Change section in product site
+function openSection(evt, sectionName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(sectionName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
